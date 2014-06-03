ed810e044e96a355c9ffec8b35e3b3fd440372f4
harttle <yangjvn@126.com>
2014-06-03 16:54:45 +0800

    modified:   .gitignore 	new file:   Gemfile 	modified:   _config.yml 	modified:   serve.sh 	deleted:    sitemap.xml

--- serve.sh
+++ serve.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-jekyll serve -w --draft 
+bundle exec jekyll serve -w --draft 
 
-#jekyll build -w --draft & 
-
-#function kill_jobs() {
-#  jobs -p | xargs kill
-#}
-#
-#trap kill_jobs EXIT
