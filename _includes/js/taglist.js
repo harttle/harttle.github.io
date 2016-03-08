7bd49e69f9b7d746321bd01dbd4e164c01da38a7
harttle <yangjvn@126.com>
2016-03-08 21:08:48 +0800

    highlight site-nav toggle when hover

--- _includes/js/taglist.js
+++ _includes/js/taglist.js
@@ -1,33 +1,39 @@
-
-window.modules.taglist = function(console, $ele, name){
+window.modules.taglist = function(console, $ele, name) {
     var $li = $ele.find('li');
 
     quicksort(0, $li.length);
     $ele.fadeIn();
 
-    function swap(i, j){
-        var l1 = $li.eq(i), l2 = $li.eq(j), tmp = l1.html();
-        l1.html(l2.html());
-        l2.html(tmp);
+    function swap(i, j) {
+        var l1 = $li.eq(i),
+            l2 = $li.eq(j),
+                tmp = l1.html();
+                l1.html(l2.html());
+                l2.html(tmp);
     }
-    function val(i){
+
+    function val(i) {
         return parseInt($li.eq(i).find('span').html().trim());
     }
-    function text(i){
+
+    function text(i) {
         return $li.eq(i).find('a').html().trim();
     }
-    function quicksort(begin, end){
-        if(end-begin<=1) return;
 
-        var ran = Math.floor(Math.random()*(end-begin)) + begin;
-        swap(ran, end-1);
+    function quicksort(begin, end) {
+        if (end - begin <= 1) return;
 
-        var pivot = end-1, lessEnd = begin, vpivot = val(pivot);
-        for(var i=begin; i<end-1; i++){
-            if(val(i) > vpivot) swap(i, lessEnd++);
-        }
-        swap(pivot, lessEnd);
-        quicksort(begin, lessEnd);
-        quicksort(lessEnd+1, end);
+        var ran = Math.floor(Math.random() * (end - begin)) + begin;
+        swap(ran, end - 1);
+
+        var pivot = end - 1,
+            lessEnd = begin,
+                vpivot = val(pivot);
+                for (var i = begin; i < end - 1; i++) {
+                    if (val(i) > vpivot) swap(i, lessEnd++);
+                }
+                swap(pivot, lessEnd);
+                quicksort(begin, lessEnd);
+                quicksort(lessEnd + 1, end);
     }
 };
