5092545c44c4152169b72b833ed921fe3e9ff49e
harttle <yangjvn@126.com>
2016-05-31 15:01:17 +0800

    remove useless bin/ to Gists

--- bin/sync.sh
+++
@@ -1,6 +0,0 @@
-git checkout coding-pages && \
-echo '[harttle] Merging master' && \
-git merge master -m 'merged master'&& \
-echo '[harttle] Pushing to coding.net' && \
-git push coding.net && \
-git checkout master
