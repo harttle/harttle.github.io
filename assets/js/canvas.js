de35f71b5ce484d3331e06100df5f5f36f06f996
harttle <yangjvn@126.com>
2018-04-22 14:06:20 +0800

    more gravity

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,10 +1,7 @@
 (function () {
     var requestAnimFrame = getAnimationFrame();
     var G = 1;
-    var resistance = 0.9;
-    var mouseX = 0;
-    var mouseY = 0;
-    var FMOUSE = 100;
+    var resistance = 0.8;
     var canvas = document.querySelector('canvas#universe');
     var container = document.querySelector('.jumbotron');
     container.style.background = 'none';
@@ -14,24 +11,21 @@
     var ctx = canvas.getContext('2d');
     var sun = {
         m: 1e5,
-        r: 15,
-        sign: 1,
+        r: R(1e5),
         color: 'white',
         l: [range(35, canvas.width - 35), range(35, canvas.height - 35)],
         v: [range(-0.1, 0.1), range(-0.1, 0.1)]
     };
     var p1 = {
         m: 1e2,
-        r: 7,
-        sign: 1,
+        r: R(1e2),
         color: '#ec232f',
         l: [sun.l[0], sun.l[1] - 25],
         v: [-6, 0]
     };
     var p2 = {
         m: 1e3,
-        r: 10,
-        sign: 1,
+        r: R(1e3),
         color: '#32ee22',
         l: [sun.l[0], sun.l[1] + 120],
         v: [3.9, 0]
@@ -40,24 +34,18 @@
 
     render();
 
-    container.addEventListener('mousemove', function (e) {
-        mouseX = e.offsetX;
-        mouseY = e.offsetY;
-    });
-    container.addEventListener('mouseout', function () {
-        mouseY = mouseX = 0;
-    });
     container.addEventListener('click', function (e) {
         var scrolling = (document.scrollingElement || document.documentElement);
         var y = e.clientY + scrolling.scrollTop - container.offsetTop;
-        var r = range(5, 25);
+        var m = Math.pow(10, range(2, 6));
+        var r = R(m);
+        var v = 10 / Math.log(m);
         stars.push({
+            m: m,
             r: r,
-            m: range(10, 1000),
             color: getRandomColor(),
             l: [e.clientX, y],
-            sign: Math.sign(Math.random() - 0.5),
-            v: [range(-0.5, 0.5), range(-0.5, 0.5)]
+            v: randomDirection(v)
         });
     });
     window.addEventListener('resize', function () {
@@ -65,6 +53,13 @@
         canvas.height = container.clientHeight;
     });
 
+    function R (m) {
+        return Math.log(m);
+    }
+    function randomDirection (v) {
+        var alpha = Math.random() * 2 * Math.PI;
+        return [Math.cos(alpha) * v, Math.sin(alpha) * v];
+    }
     function range (min, max) {
         return Math.random() * (max - min) + min;
     }
@@ -84,10 +79,15 @@
             star.f = [0, 0];
         });
         for (var i = 0; i < stars.length; i++) {
-            applyMouse(stars[i]);
             for (var j = i + 1; j < stars.length; j++) {
-                var fg = Fg(stars[i], stars[j]);
-                applyF(stars[i], stars[j], fg);
+                var die = applyG(i, j);
+                if (die === i) {
+                    eat(j, i--);
+                    if (j > i) j--;
+                    break;
+                } else if (die === j) {
+                    eat(i, j--);
+                }
             }
         }
         stars.forEach(function (star) {
@@ -97,42 +97,43 @@
         });
         requestAnimFrame(render);
     }
+    function eat (i, j) {
+        var si = stars[i];
+        var sj = stars[j];
+        var m = si.m + sj.m;
+        si.v[0] = (si.v[0] * si.m + sj.v[0] * sj.m) / m;
+        si.v[1] = (si.v[1] * si.m + sj.v[1] * sj.m) / m;
+        si.m = m;
+        si.r = R(m);
+        stars.splice(j, 1);
+    }
     function accelerate (star) {
         star.v[0] += star.f[0] / star.m;
         star.v[1] += star.f[1] / star.m;
     }
-    function applyF (lhs, rhs, fg) {
-        var sign = lhs.sign * rhs.sign;
-        lhs.f[0] += fg[0] * sign;
-        lhs.f[1] += fg[1] * sign;
-        rhs.f[0] -= fg[0] * sign;
-        rhs.f[1] -= fg[1] * sign;
-    }
-    function applyMouse (star) {
-        if (mouseX === 0 || mouseY === 0) {
-            return;
-        }
-        var lx = star.l[0] - mouseX;
-        var ly = star.l[1] - mouseY;
-        var lx2 = lx * lx;
-        var ly2 = ly * ly;
-        var r2 = Math.max(lx2 + ly2, 1);
-        var f = FMOUSE * star.m * star.m / r2;
-        var fx = Math.sign(lx) * Math.sqrt(f * lx2 / r2);
-        var fy = Math.sign(ly) * Math.sqrt(f * ly2 / r2);
-        star.f[0] += fx;
-        star.f[1] += fy;
-    }
-    function Fg (lhs, rhs) {
+    function applyG (i, j) {
+        var lhs = stars[i];
+        var rhs = stars[j];
         var lx = rhs.l[0] - lhs.l[0];
         var ly = rhs.l[1] - lhs.l[1];
         var lx2 = lx * lx;
         var ly2 = ly * ly;
-        var r2 = Math.max(lx2 + ly2, 1);
+        var r2 = Math.max(lx2 + ly2, 0.01);
+        var r = Math.sqrt(r2);
+
+        if (r < 5) {
+            return lhs.m > rhs.m ? j : i;
+        }
+
         var f = G * lhs.m * rhs.m / r2;
         var fx = Math.sign(lx) * Math.sqrt(f * lx2 / r2);
         var fy = Math.sign(ly) * Math.sqrt(f * ly2 / r2);
-        return [fx, fy];
+
+        lhs.f[0] += fx;
+        lhs.f[1] += fy;
+        rhs.f[0] -= fx;
+        rhs.f[1] -= fy;
+        return -1;
     }
     function draw (star) {
         ctx.beginPath();
@@ -144,10 +145,10 @@
     function nextMove (star) {
         var x = star.l[0] + star.v[0];
         var y = star.l[1] + star.v[1];
-        if (x > canvas.width - star.r || x < star.r) {
+        if (x > canvas.width + 100 || x < -100) {
             star.v[0] = -star.v[0] * resistance;
         }
-        if (y > canvas.height - star.r || y < star.r) {
+        if (y > canvas.height + 100 || y < -100) {
             star.v[1] = -star.v[1] * resistance;
         }
 
